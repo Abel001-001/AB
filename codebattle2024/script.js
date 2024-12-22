d2a47c1c47a3fbcf4f497af39aa8d34ddc80db44
Abel001-001 <abelisrayelyan07@gmail.com>
2024-12-23 01:31:44 +0400

    Add files via upload

---
+++ codebattle2024/script.js
@@ -0,0 +1,12 @@
+
+
+var a = document.getElementById('bars');
+
+function bars() {
+	sidebar.style.right = "0px";
+	a.style.display = "none"
+}
+function barsoff(){
+	sidebar.style.right = "100%"
+	a.style.display = "block"
+}
